8b8f0bf5331cc00e3f4a538764c4c40fb2bd7e70
praseedhamaddipatla <praseedha.maddipatla@gmail.com>
2026-01-18 13:03:12 -0600

    files created

---
+++ FloatingPoint.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#define SIGN 1
+#define EXP_SINGLE 8
+#define FRAC_SINGLE 23
+#define EXP_DOUBLE 11
+#define FRAC_DOUBLE 52
+#define BIAS 127
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cs429Prog1FloatingPoint
 Conversion between decimal floating-point number and IEEE 754 representation
+
+Name: Praseedha Maddipatla
+EID: psm2357
